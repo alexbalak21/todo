45a0468d91c3c94c5b72bda55d5ba3f8d47fb2e4
alexblack24 <alex.black@mail.com>
2024-04-23 12:14:02 +0200

    create don, read done

--- db.js
+++ db.js
@@ -1,34 +1,76 @@
-const mysql = require("mysql")
+const mysql = require("mysql2/promise")
 
-function connect_to_db() {
-    let con = mysql.createConnection({
+async function connect_to_db() {
+    const connection = await mysql.createConnection({
         host: "localhost",
         user: "root",
         password: "",
         database: "todo",
+        waitForConnections: true,
+        connectionLimit: 100,
+        queueLimit: 0,
     })
-    con.connect(function (err) {
-        if (err) throw err
-        // else console.log("Connected")
-        else return con
+    connection.addListener("error", (err) => {
+        if (err instanceof Error) throw err
     })
+    return connection
 }
 
-function query_db(con) {
-    const sql_query = "CREATE TABLE totos (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), description TEXT)"
-    con.query(sql_query, function (err, result) {
-        if (err) throw err
-        console.log("Table created")
-    })
-}
-
-function main() {
+async function create_todo(name = "name", description = "description") {
     try {
-        const conn = connect_to_db()
-        query_db(conn)
+        const connection = await connect_to_db()
+        const result = await connection.execute("INSERT INTO todos (name, description) VALUES (?, ?)", [
+            `${name}`,
+            `${description}`,
+        ])
+        connection.unprepare()
+        connection.end()
+        console.log(result)
+    } catch (error) {
+        throw error
+    }
+}
+
+async function read_todos() {
+    try {
+        const connection = await connect_to_db()
+        const [rows] = await connection.execute("SELECT * FROM todos")
+        console.log(rows)
+        return rows
     } catch (error) {
         console.log(error)
     }
 }
 
-main()
+function query_db() {
+    try {
+        const connection = connect_to_db()
+        connection.prepare()
+    } catch (error) {
+        throw error
+    }
+    // const sql_query = "CREATE TABLE todos (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), description TEXT)"
+    // con.query("DROP TABLE todos")
+    con.query(sql_query)
+    console.log("Table created")
+    con.query("INSERT INTO todos (name, description) VALUES ('First Task', 'description 1')")
+    console.log("INSERT DONE")
+    // con.query("DROP TABLE todos")
+    // console.log("TABLE DROPED")
+}
+
+async function select_all(CONN) {
+    const [rows, fields] = await CONN.query("SELECT * FROM todos")
+    return [rows, fields]
+}
+
+async function main() {
+    const conn = await connect_to_db()
+    console.log("DATABASE CONNECTED")
+    const result = await select_all(conn)
+    console.log(result[0])
+    console.log("DONE")
+    conn.end()
+}
+
+read_todos()
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2/promise")
 
 async function connect_to_db() {
-    return await mysql.createConnection({
+    const connection = await mysql.createConnection({
         host: "localhost",
         user: "root",
         password: "",
@@ -10,10 +10,37 @@ async function connect_to_db() {
         connectionLimit: 100,
         queueLimit: 0,
     })
+    connection.addListener("error", (err) => {
+        if (err instanceof Error) throw err
+    })
+    return connection
 }
 
-function query_db(con) {
-    const sql_query = "CREATE TABLE todos (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), description TEXT)"
+async function create_todo(name = "name", description = "description") {
+    try {
+        const connection = await connect_to_db()
+        const result = await connection.execute("INSERT INTO todos (name, description) VALUES (?, ?)", [
+            `${name}`,
+            `${description}`,
+        ])
+        connection.unprepare()
+        connection.end()
+        console.log(result)
+    } catch (error) {
+        throw error
+    }
+}
+
+async function read_todos() {}
+
+function query_db() {
+    try {
+        const connection = connect_to_db()
+        connection.prepare()
+    } catch (error) {
+        throw error
+    }
+    // const sql_query = "CREATE TABLE todos (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), description TEXT)"
     // con.query("DROP TABLE todos")
     con.query(sql_query)
     console.log("Table created")
